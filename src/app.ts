0f63e7f1b4cdf1971793e92fbc1d5936a8597205
Uri <uri555459@gmail.com>
2024-01-19 08:41:54 +0300

    Base typescript template

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import { str } from './str/str'
+
+const app = (a: number, b: number): number => {
+	return a + b
+}
+
+console.log(str('hello', 'world'))
+
+app(1, 2)
---
+++ src/str/str.ts
@@ -0,0 +1,1 @@
+export const str = (a: string, b: string): string => a + b
